db43e64058bcd9a8e7b25ae2247b4072159def8f
101zh <67253838+101zh@users.noreply.github.com>
2026-02-09 17:14:22 -0800

    add game design meeting 8 code

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2D;
+    BoxCollider2D coll2D;
+    [SerializeField] LayerMask jumpableSurface;
     [SerializeField] float movementSpeed = 9.0f;
+    [SerializeField] float jumpForce = 6.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        coll2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,8 +24,18 @@ public class PlayerController : MonoBehaviour
         Debug.Log(inputX);
 
         Vector2 velocity = new Vector2(inputX * movementSpeed, rb2D.velocity.y);
-        
+
         rb2D.velocity = velocity;
-        
+
+        Debug.Log(isOnGround());
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround())
+        {
+            rb2D.AddForce(new Vector2(0, 1) * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool isOnGround()
+    {
+        return Physics2D.BoxCast(coll2D.bounds.center, coll2D.bounds.size, 0f, Vector2.down, 0.1f, jumpableSurface);
     }
 }
